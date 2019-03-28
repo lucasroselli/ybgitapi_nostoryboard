64422364ab2164aa9b259ced7620e32620b9159c
Lucas Leonardi Roselli <lucasleonardiroselli@Lucass-MacBook-Pro.local>
2019-03-28 20:21:48 -0300

    infinitive scroll

--- noStory/Views/ListGitTableViewController.swift
+++ noStory/Views/ListGitTableViewController.swift
@@ -3,6 +3,7 @@ import UIKit
 class ListGitTableViewController: UITableViewController {
     
     var users : [UserGit] = []
+    var page = 1
     
     override var preferredStatusBarStyle: UIStatusBarStyle{
         return .lightContent
@@ -15,11 +16,34 @@ class ListGitTableViewController: UITableViewController {
         self.view.backgroundColor = UIColor.groupTableViewBackground
         
         //chamada http
-        let urlString = "https://api.github.com/search/users?q=followers:%3E10000"
+        callApi()
+    }
+    
+    var carregando = false
+    override func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
         
+        if carregando == true{
+            return
+        }
+        
+        if indexPath.row > (self.users.count - 4){
+            print("chegoue \(indexPath.row)")
+            carregando = true
+            page = page + 1
+            
+            callApi()
+        }
+    }
+    
+    func callApi(){
+        let urlString = "https://api.github.com/search/users?q=followers:%3E1000&page=\(page)&per_page=40"
+        
+        UIApplication.shared.isNetworkActivityIndicatorVisible = true
         HttpHandler.Call(urlString: urlString) { (res : GitRS?) in
-            self.users = res!.items            
+            UIApplication.shared.isNetworkActivityIndicatorVisible = false
+            self.users.append(contentsOf: res!.items)
             DispatchQueue.main.async {
+                self.carregando = false
                 self.title = "Maiores do Git"
                 self.tableView?.reloadData()
             }
